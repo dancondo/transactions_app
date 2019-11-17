cb5e41c1f0e3b1ea513e84072fa7b424a902b35a
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2019-11-17 16:45:00 -0300

    Add locale and create routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,119 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
+import './new_transaction.dart';
 import './transaction.dart';
 
-void main() => initializeDateFormatting('pt_BR', null).then((_) => runApp(MyApp()));
+void main() =>
+    initializeDateFormatting('pt_BR', null).then((_) => runApp(MyApp()));
 
 class MyApp extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     Intl.defaultLocale = 'pt_BR';
     return MaterialApp(
-      title: 'Despesas App',
+      title: 'Minhas Ações App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Despesas App'),
       locale: Locale('pt_BR'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MyHomePage(),
+        '/new_transaction': (context) => NewTransactionPage(),
+      },
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
   final List<Transaction> transactions = [
     Transaction(
       id: 'fda321ed',
-      title: 'Uber',
-      value: 5.32,
+      name: 'BBDC4',
+      value: 35.50,
+      quantity: 84,
       date: DateTime.now(),
     ),
     Transaction(
       id: 'kakaskakakfdaw2',
-      title: 'Camiseta',
-      value: 40.00,
+      name: 'BIDI4',
+      value: 19.15,
+      quantity: 100,
       date: DateTime.now(),
     )
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text('Placeholder'),
-                elevation: 5,
-              ),
+      appBar: AppBar(
+        title: Text('Meus Ativos'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Placeholder'),
+              elevation: 5,
             ),
-            Column(
-              children: transactions.map((transaction) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.orange,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${transaction.value}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.orange,
-                          ),
+          ),
+          Column(
+            children: transactions.map((transaction) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.orange,
+                          width: 2,
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transaction.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                            ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'R\$ ${transaction.value}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.orange,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          transaction.name,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
                           ),
-                          Text(
-                            new DateFormat.yMMMMd().format(transaction.date),
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            )
-          ],
-        ));
+                        ),
+                        Text(
+                          new DateFormat.yMd().format(transaction.date),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/new_transaction');
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
