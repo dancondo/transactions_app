1678a2f046901f7b0fb818f30d80c6b2c0098182
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2019-11-17 16:45:09 -0300

    add new fields to transaction class

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -2,14 +2,16 @@ import 'package:flutter/foundation.dart';
 
 class Transaction {
   final String id;
-  final String title;
+  final String name;
   final double value;
+  final int quantity; 
   final DateTime date;
 
   Transaction({
     @required this.id,
-    @required this.title,
+    @required this.name,
     @required this.value,
+    @required this.quantity,
     @required this.date,
   });
 }
