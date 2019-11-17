d6e8e034aedf6e4a5f8d5f07182ca332cc4461a3
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2019-11-17 17:15:02 -0300

    Move files into folders

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import './new_transaction.dart';
-import './transaction.dart';
+import '../models/transaction.dart';
 
 void main() =>
     initializeDateFormatting('pt_BR', null).then((_) => runApp(MyApp()));
