d97e1dbafeb973a4e292e1056036804253fe30f1
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2019-11-17 17:12:49 -0300

    style and form fields

--- lib/new_transaction.dart
+++ lib/new_transaction.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class NewTransactionPage extends StatelessWidget {
+  final nameController = TextEditingController();
+  final dateController = TextEditingController();
+  final amountController = TextEditingController();
+  final priceController = TextEditingController();
+
+  _save() {
+    return null;
+  }
+
+  _clear() {
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,16 +29,39 @@ class NewTransactionPage extends StatelessWidget {
               decoration: InputDecoration(
                 labelText: 'Ativo',
               ),
+              controller: nameController,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Data de Compra',
+              ),
+              controller: dateController,
             ),
             TextField(
               decoration: InputDecoration(
                 labelText: 'Quantidade',
               ),
+              controller: amountController,
             ),
             TextField(
               decoration: InputDecoration(
                 labelText: 'Preço Unitário',
               ),
+              controller: priceController,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                RaisedButton(
+                  child: Text('Limpar'),
+                  onPressed: _clear,
+                ),
+                RaisedButton(
+                  child: Text('Enviar'),
+                  onPressed: _save,
+                  color: Colors.orange,
+                ),
+              ],
             ),
           ],
         ),
